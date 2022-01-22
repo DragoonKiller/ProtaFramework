ad9063bb6374f847a4b09a1b2a706b13999d2fe9
Narellanda Karatoga <karatoga@qq.com>
2022-01-23 00:03:39 +0800

    [Prota] LuaInspector opt

--- Lua/Editor/LuaScriptInspector.cs
+++ Lua/Editor/LuaScriptInspector.cs
@@ -13,19 +13,26 @@ namespace Prota.Lua
     {
         LuaElementInspector luaInspector;
         
+        ObjectField scriptField;
+        
+        TextField pathText;
+        
         public override VisualElement CreateInspectorGUI()
         {
             var root = new VisualElement();
             
-            root.AddChild(new ObjectField("脚本文件") {
+            root.AddChild(scriptField = new ObjectField("文件") {
                     objectType = typeof(TextAsset),
                     value = serializedObject.FindProperty("script").objectReferenceValue
                 }
                 .OnValueChange<ObjectField, UnityEngine.Object>(e => {
                     serializedObject.FindProperty("script").objectReferenceValue = e.newValue;
+                    RefreshPathLabel();
                     EditorUtility.SetDirty(serializedObject.targetObject);
                 })
             );
+            
+            root.AddChild(pathText = new TextField("路径").SetNoInteraction());
             
             root.AddChild(luaInspector = new LuaElementInspector(0, "self", () => {
                 var obj = serializedObject.targetObject as LuaScript;
@@ -33,6 +40,8 @@ namespace Prota.Lua
                 var table = obj.instance;
                 return table;
             }));
+            
+            RefreshPathLabel();
             
             return root;
         }
@@ -52,6 +61,19 @@ namespace Prota.Lua
             luaInspector?.Update();
         }
         
+        void RefreshPathLabel()
+        {
+            var v = scriptField.value as TextAsset;
+            if(v == null)
+            {
+                pathText.value = "没有指定文件.";
+            }
+            else
+            {
+                var path = AssetDatabase.GetAssetPath(v);
+                pathText.value = path;
+            }
+        }
         
     }
 }
