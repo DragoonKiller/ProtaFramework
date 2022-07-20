420dbfde214317471b791ca218faaefafd2d47b4
karatoga <karatoga@qq.com>
2022-07-20 15:58:43 +0800

    [Prota] TexGen

---
+++ Procedural/Texture/Grid.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+namespace Prota.Procedural
+{
+    public partial class ProceduralTexture
+    {
+        static Texture2D Grid(int w, int h)
+        {
+            w += 1;
+            h += 1;
+            var res = new Texture2D(w, h, TextureFormat.RGBA32, 0, true, true);
+            var data = new Color32[w * h];
+            for(int i = 0; i < h; i++) for(int j = 0; j < w; j++)
+            {
+                var b = i * w + j;
+                var ia = i % 32;
+                var ib = j % 32;
+                data[b] = ia == 0 || ib == 0 || ia == 1 || ib == 1 || ia == 31 || ib == 31
+                    ? new Color32(0xFF, 0xFF, 0xFF, 0xFF)
+                    : new Color32(0, 0, 0, 0xFF);
+                    
+            }
+            res.SetPixelData(data, 0);
+            res.filterMode = FilterMode.Point;
+            res.Apply();
+            return res;
+        }
+    }
+}
---
+++ Procedural/Texture/ProceduralTexture.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace Prota.Procedural
+{
+    [ExecuteAlways]
+    public partial class ProceduralTexture : MonoBehaviour
+    {
+        public enum TextureType
+        {
+            None,
+            Grid,
+        }
+        
+        public TextureType type;
+        
+        [SerializeField] TextureType recordType;
+        
+        [SerializeField] Texture texture;
+        
+        [SerializeField] Sprite sprite;
+        
+        void Start()
+        {
+            
+        }
+        
+        void Update()
+        {
+            if(recordType != type)
+            {
+                recordType = type;
+                if(texture)
+                {
+                    DestroyImmediate(texture);
+                    DestroyImmediate(sprite);
+                }
+                if(type == TextureType.None) { }
+                else if(type == TextureType.Grid) texture = Grid(512, 512);
+                else Debug.LogError("Wrong texture type.");
+                
+                if(texture)
+                {
+                    texture.name = "Generated";
+                }
+                if(texture is Texture2D t)
+                {
+                    sprite = Sprite.Create(t, new Rect(0, 0, t.width, t.height), new Vector2(0.5f, 0.5f), 128, 0, SpriteMeshType.FullRect);
+                    sprite.name = "Generated";
+                }
+            }
+            
+            if(UnityEngine.Application.isPlaying)
+            {
+                if(TryGetComponent<SpriteRenderer>(out var cc))
+                {
+                    cc.sprite = sprite;
+                }
+            }
+        }
+    }
+}
