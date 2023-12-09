055394c7af9cf8ab17c8f6bab76fe71eec6d1270
Narellanda Karatoga <karatoga@qq.com>
2023-12-09 18:32:39 +0800

    存档系统

--- Unity/Utils/Save.cs
+++ Unity/Utils/Save.cs
@@ -5,8 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
-using System.Threading;
-using JetBrains.Annotations;
 
 namespace Prota.Unity
 {
@@ -17,42 +15,58 @@ namespace Prota.Unity
         public string value;
     }
     
+    [Serializable]
+    class SaveJson
+    {
+        [SerializeField] public List<SaveEntry> list;
+    }
+    
     // 简简单单存档管理类.
     public class Save : Singleton<Save>
     {
-        public static DirectoryInfo dir = new DirectoryInfo(Application.persistentDataPath);
+        public static DirectoryInfo dir;
+        public static FileInfo saveFile;
+        public static FileInfo backupFile;
         
-        public static FileInfo saveFile = dir.SubFile("save.json");
-        
-        public static FileInfo backupFile = dir.SubFile("save.json.back");
+        public bool dataLoaded { get; private set; }
         
         // 数据更新了, 需要保存.
-        [SerializeField, Readonly] bool pending = true;
+        bool pending = true;
         
         // 数据正在保存中.
-        [SerializeField, Readonly] bool saving = false;
+        bool saving = false;
         
         readonly Dictionary<string, string> data = new Dictionary<string, string>();
         
         // ====================================================================================================
         // ====================================================================================================
         
+        [InitializeOnLoadMethod]
+        [RuntimeInitializeOnLoadMethod]
+        static void Init()
+        {
+            dir = new DirectoryInfo(Application.persistentDataPath);
+            saveFile = dir.SubFile("save.json");
+            backupFile = dir.SubFile("save.json.back");
+        }
+        
         protected override void Awake()
         {
             base.Awake();
-            
             dir.EnsureExists();
             if (!saveFile.Exists) saveFile.Create().Dispose();
-            ReadData();
+            LoadData();
         }
         
-        void ReadData()
+        public void LoadData()
         {
-            using var _ = TempList.Get<SaveEntry>(out var entries);
             var json = File.ReadAllText(saveFile.FullName);
-            JsonUtility.FromJsonOverwrite(json, entries);
+            Debug.LogError(json);
+            var jsonObject = JsonUtility.FromJson<SaveJson>(json);
+            Debug.LogError(JsonUtility.ToJson(jsonObject));
             data.Clear();
-            foreach (var entry in entries) data[entry.key] = entry.value;
+            foreach (var entry in jsonObject.list) data[entry.key] = entry.value;
+            dataLoaded = true;
         }
         
         void SaveInstantly()
@@ -65,7 +79,9 @@ namespace Prota.Unity
                 entries.Add(new SaveEntry { key = kv.Key, value = kv.Value });
             }
             
-            var json = JsonUtility.ToJson(entries);
+            var jsonObject = new SaveJson { list = entries };
+            
+            var json = JsonUtility.ToJson(jsonObject, true);
             
             File.WriteAllText(saveFile.FullName, json);
         }
@@ -76,17 +92,24 @@ namespace Prota.Unity
             saveFile.CopyTo(backupFile.FullName);
         }
         
-        void LateUpdate()
+        public void Update()
         {
             if(pending)
             {
                 if(!saving)
                 {
                     pending = false;
+                    saving = true;
                     Task.Run(() => {
-                        saving = true;
-                        SaveInstantly();
-                        saving = false;
+                        try
+                        {
+                            SaveInstantly();
+                            saving = false;
+                        }
+                        catch(Exception e)
+                        {
+                            Debug.LogException(e);
+                        }
                     });
                 }
                 else
@@ -106,54 +129,108 @@ namespace Prota.Unity
         // ====================================================================================================
         // ====================================================================================================
         
-        
-        public void Write<T>(string key, T value)
+        public bool Erase(string key)
         {
             key = ToStandardKey(key);
-            data[key] = JsonUtility.ToJson(value);
+            var res = data.Remove(key);
+            pending = true;
+            return res;
+        }
+        
+        public void Write(string key, string value)
+        {
+            key = ToStandardKey(key);
+            data[key] = value;
             pending = true;
         }
         
-        public bool Read<T>(string key, out T value) where T: struct
+        public bool Read(string key, out string value)
         {
             key = ToStandardKey(key);
-            
-            if (data.TryGetValue(key, out var json))
+            if (data.TryGetValue(key, out var str))
             {
-                value = JsonUtility.FromJson<T>(json);
+                value = str;
                 return true;
             }
-            
             value = default;
             return false;
         }
         
-        public bool Read<T>(string key, T target) where T: class
+        
+        // ====================================================================================================
+        // ====================================================================================================
+        
+        public static bool Get(string key, out int value, int defaultValue = default)
         {
-            if(target == null) return false;
-            
-            key = ToStandardKey(key);
-            
-            if (data.TryGetValue(key, out var json))
-            {
-                JsonUtility.FromJsonOverwrite(json, target);
-                return true;
-            }
-            
-            return false;
+            var res = instance.Read(key, out string str);
+            value = res ? int.Parse(str) : defaultValue;
+            return res;
         }
+        
+        public static bool Get(string key, out float value, float defaultValue = default)
+        {
+            var res = instance.Read(key, out string str);
+            value = res ? float.Parse(str) : defaultValue;
+            return res;
+        }
+        
+        public static bool Get(string key, out long value, long defaultValue = default)
+        {
+            var res = instance.Read(key, out string str);
+            value = res ? int.Parse(str) : defaultValue;
+            return res;
+        }
+        
+        public static bool Get(string key, out double value, double defaultValue = default)
+        {
+            var res = instance.Read(key, out string str);
+            value = res ? double.Parse(str) : defaultValue;
+            return res;
+        }
+        
+        public static bool Get(string key, out bool value, bool defaultValue = default)
+        {
+            var res = instance.Read(key, out string str);
+            value = res ? bool.Parse(str) : defaultValue;
+            return res;
+        }
+        
+        public static bool Get(string key, out string value, string defaultValue = default)
+        {
+            var res = instance.Read(key, out value);
+            value = res ? value : defaultValue;
+            return res;
+        }
+        
+        public static bool GetObject<T>(string key, T value) where T: class
+        {
+            var res = instance.Read(key, out var str);
+            if (res) JsonUtility.FromJsonOverwrite(str, value);
+            return res;
+        }
+        
+        
+        
+        public static bool Set<T>(string key, T value)
+        {
+            instance.Write(key, value.ToString());
+            return true;
+        }
+        
+        public static bool SetObject<T>(string key, T value) where T: class
+        {
+            var json = JsonUtility.ToJson(value);
+            instance.Write(key, json);
+            return true;
+        }
+        
+        // ====================================================================================================
+        // ====================================================================================================
+        
         
         string ToStandardKey(string a)
         {
             return a.ToLower().Replace(" ", "_");
-        }
-        
-        
-        public static void Test()
-        {
-            
-            
-            
         }
         
     }
