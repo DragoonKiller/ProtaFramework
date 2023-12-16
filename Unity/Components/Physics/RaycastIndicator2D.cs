f5fab80159d2385481524be84b8baf7f2ade7264
Narellanda Karatoga <karatoga@qq.com>
2023-12-16 17:39:16 +0800

    RaycastIndicator添加盒子射线检测

--- Unity/Components/Physics/RaycastIndicator2D.cs
+++ Unity/Components/Physics/RaycastIndicator2D.cs
@@ -7,6 +7,12 @@ using System;
 
 namespace Prota.Unity
 {
+    public enum RaycastIndicatorType
+    {
+        Line = 0,
+        Box,
+    }
+    
     [DisallowMultipleComponent]
     [ExecuteAlways]
     public class RaycastIndicator2D : MonoBehaviour
@@ -15,10 +21,25 @@ namespace Prota.Unity
         
         public LayerMask layerMask = -1;
         
+        public RaycastIndicatorType type;
+        
+        public Vector3 size;
+        
         public bool Cast(out RaycastHit2D hit)
         {
-            hit = Physics2D.Raycast(transform.position, relativePosition, relativePosition.magnitude, layerMask);
-            return hit.collider != null;
+            switch(type)
+            {
+                case RaycastIndicatorType.Line:
+                    hit = Physics2D.Raycast(transform.position, relativePosition, relativePosition.magnitude, layerMask);
+                    return hit.collider != null;
+                    
+                case RaycastIndicatorType.Box:
+                    hit = Physics2D.BoxCast(transform.position, size, 0, relativePosition, relativePosition.magnitude, layerMask);
+                    return hit.collider != null;
+                    
+                default:
+                    throw new NotImplementedException($" RaycastIndicator 2D at [{this.GetNamePath()}] :: [{ type }]");
+            }
         }
         
         
@@ -27,11 +48,22 @@ namespace Prota.Unity
         {
             var myPos = transform.position;
             ProtaDebug.DrawArrow(myPos, myPos + relativePosition.ToVec3(), Color.red);
+            if(type == RaycastIndicatorType.Box)
+            {
+                ProtaDebug.DrawBox2D(myPos, size, 0, Color.yellow);
+            }
+            
             if(Cast(out var hit))
             {
                 var hitPoint = hit.point.ToVec3(myPos.z);
                 ProtaDebug.DrawArrow(myPos, hitPoint, Color.green);
                 ProtaDebug.DrawArrow(hitPoint, hitPoint + hit.normal.ToVec3() * 0.4f, Color.blue);
+                
+                if(type == RaycastIndicatorType.Box)
+                {
+                    var hitPos = this.transform.position + hit.distance * relativePosition.normalized.ToVec3();
+                    ProtaDebug.DrawBox2D(hitPos, size, 0, Color.blue);
+                }
             }
         }
         #endif
--- Unity/Utils/ProtaDebug.cs
+++ Unity/Utils/ProtaDebug.cs
@@ -6,6 +6,26 @@ namespace Prota.Unity
 {
     public static partial class ProtaDebug
     {
+        public static void DrawBox2D(Vector3 pos, Vector2 size, float rotation, Color? color = null, float? duration = null)
+        {
+            if(color == null) color = Color.green;
+            if(duration == null) duration = 0;
+            
+            var halfSize = size / 2;
+            var bottomLeft = -halfSize;
+            var topRight = halfSize;
+            var bottomRight = new Vector2(halfSize.x, -halfSize.y);
+            var topLeft = new Vector2(-halfSize.x, halfSize.y);
+            var bottomLeftRotated = pos + bottomLeft.Rotate(rotation).ToVec3();
+            var topRightRotated = pos + topRight.Rotate(rotation).ToVec3();
+            var bottomRightRotated = pos + bottomRight.Rotate(rotation).ToVec3();
+            var topLeftRotated = pos + topLeft.Rotate(rotation).ToVec3();
+            Debug.DrawLine(bottomLeftRotated, bottomRightRotated, color.Value, duration.Value);
+            Debug.DrawLine(bottomRightRotated, topRightRotated, color.Value, duration.Value);
+            Debug.DrawLine(topRightRotated, topLeftRotated, color.Value, duration.Value);
+            Debug.DrawLine(topLeftRotated, bottomLeftRotated, color.Value, duration.Value);
+        }
+        
         public static void DrawArrow(Vector3 from, Vector3 to, Color? _color = null, float? _duration = null, float? _size = null)
         {
             var color = _color ?? Color.green;
