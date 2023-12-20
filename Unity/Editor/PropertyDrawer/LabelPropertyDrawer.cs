d9c029cd66dcc0194481004896cd497331aa0448
Narellanda Karatoga <karatoga@qq.com>
2023-12-20 17:30:25 +0800

    修改label标记

--- Unity/Editor/PropertyDrawer/LabelPropertyDrawer.cs
+++ Unity/Editor/PropertyDrawer/LabelPropertyDrawer.cs
@@ -11,11 +11,17 @@ namespace Prota.Editor
     [CustomPropertyDrawer(typeof(NameAttribute))]
     public class LabelDrawer : PropertyDrawer
     {
-        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        // public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        // {
+        //     var labelAttribute = attribute as NameAttribute;
+        //     if(labelAttribute == null) return new PropertyField(property);
+        //     return new PropertyField(property) { label = labelAttribute.name, tooltip = property.name };
+        // }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             var labelAttribute = attribute as NameAttribute;
-            if(labelAttribute == null) return new PropertyField(property);
-            return new PropertyField(property) { label = labelAttribute.name, tooltip = property.name };
+            base.OnGUI(position, property, new GUIContent(labelAttribute.name));
         }
     }
 }
